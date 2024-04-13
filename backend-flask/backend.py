a6503d5f946f0bb99387524fa3f3c70cb7e1eb7b
ibsina <ibsina@gmail.com>
2024-04-13 20:17:31 +0800

    upload

---
+++ backend-flask/backend.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify,render_template,redirect, url_for
+import requests
+from flask_cors import CORS
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST']='172.19.8.150'
+app.config['MYSQL_USER']='cbc'
+app.config["MYSQL_PASSWORD"]='fortinet'
+app.config["MYSQL_DB"]='flaskapp'
+
+mysql=MySQL(app)
+CORS(app)
+
+@app.route("/registration",methods=['GET','POST'])
+@app.route("/registration/",methods=['GET','POST'])
+def registration():
+	cur=mysql.connection.cursor()
+	data=request.get_json()
+	print("JSON:")
+	print(data)
+	firstname = data['firstname']
+	lastname = data['lastname']
+	email = data['email']
+	username = data['username']
+	password = data['password']
+	print(firstname,lastname,email,username,password)
+	query="INSERT INTO users VALUES('{}','{}','{}','{}','{}')".format(firstname,lastname,email,username,password)
+	print(query)
+	cur.execute(query)
+	cur.execute("SELECT * FROM users")
+	fetchdata=cur.fetchall()
+	print(fetchdata)
+	mysql.connection.commit()
+	cur.close()
+	return data
+
+@app.route("/login",methods=['GET','POST'])
+@app.route("/login/",methods=['GET','POST'])
+def login():
+	cur=mysql.connection.cursor()
+	data=request.get_json()
+	username = data['username']
+	password = data['password']
+	query="select username from users where username = '" + username + "' and password = '" + password + "';"
+	cur.execute(query)
+	row = cur.fetchone()
+	mysql.connection.commit()
+	cur.close()
+	if row is not None:
+		return jsonify(result="true")
+	else:
+		return jsonify(result="false")
+
+if __name__ == "__main__":
+    app.run(debug= False,port=5000,host='0.0.0.0')
---
+++ frontend-html/front.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify,render_template,redirect, jsonify, Response
+from flask_cors import CORS
+import requests
+import json
+
+app = Flask(__name__)
+CORS(app)
+here=""
+@app.route('/')
+def form():
+    return render_template('index.html')
+
+@app.route('/login')
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/registration')
+@app.route('/registration/')
+def registration():
+    return render_template('registration.html')
+
+@app.route("/addrec", methods=['GET','POST'])
+@app.route("/addrec/", methods=['GET','POST'])
+def addrec():
+	if request.method == 'POST':
+		here=request.form
+		Headers = { 'FSI_APIKEY' : '37JFybUZjpojWEa9GzCM536RGkw56RutMPYu8Vj8' }
+		response = requests.post('http://seahk-is-fwb-internal-5cf4808a9027dc93.elb.ap-southeast-1.amazonaws.com/registration',headers = Headers, json=dict(here))
+
+	if response.status_code == 200:
+		return render_template('thanks.html')
+	else:
+		return Response(response.content, response.status_code)
+
+@app.route("/chkrec", methods=['GET','POST'])
+@app.route("/chkrec/", methods=['GET','POST'])
+def chkrec():
+	if request.method == 'POST':
+		here=request.form
+		Headers = { 'FSI_APIKEY' : '37JFybUZjpojWEa9GzCM536RGkw56RutMPYu8Vj8' }
+		response = requests.post('http://seahk-is-fwb-internal-5cf4808a9027dc93.elb.ap-southeast-1.amazonaws.com/login',headers = Headers, json=dict(here))
+
+	if response.status_code == 200:
+		response_dict = json.loads(response.text)
+		result = response_dict["result"]
+		if result == "true":
+			return render_template('success.html')
+		else:
+			return render_template('fail.html')
+	else:
+		return Response(response.content, response.status_code)
+
+if __name__ == "__main__":
+    app.run(debug= False,port=5001,host="0.0.0.0")
